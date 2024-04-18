b1742c850102ccd348b311f89bd60afddca57b76
Alex Hughes <alxhghs@gmail.com>
2024-04-18 08:24:26 -0700

    react query example

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+    QueryClientProvider,
+    QueryClient,
+    useQuery,
+} from "@tanstack/react-query";
+import { useReactQueryDevTools } from "@dev-plugins/react-query";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const client = new QueryClient();
+    useReactQueryDevTools(client);
+    return (
+        <QueryClientProvider client={client}>
+            <View style={styles.container}>
+                <Text>Open up App.tsx to start working on your app!</Text>
+                <Child />
+                <StatusBar style="auto" />
+            </View>
+        </QueryClientProvider>
+    );
 }
 
+const Child = () => {
+    const { data } = useQuery({
+        queryKey: ["coffee", "hot"],
+        queryFn: async () => {
+            return await fetch("https://api.sampleapis.com/coffee/hot");
+        },
+    });
+    return <Text>{JSON.stringify(data)}</Text>;
+};
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
